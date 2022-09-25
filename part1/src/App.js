2047dacb8499522ea8405867a11d139a15342737
Andy-git985 <andy.yu617@gmail.com>
2022-09-25 16:24:54 -0400

    Passing state to child components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,26 @@
-// const Footer = () => {
-//   return (
-//     <div>
-//       greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-//     </div>
-//   );
-// };
-
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age;
-
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   const name = 'Peter';
-//   const age = 10;
-//   return (
-//     <>
-//       <div>
-//         <h1>Greetings</h1>
-//         <Hello name="Maya" age={26 + 10} />
-//         <Hello name={name} age={age} />
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// };ss
-
-// Page re-rendering
-// const App = (props) => {
-//   const { counter } = props;
-//   return <div>{counter}</div>;
-// };
-
-// Stateful component
 import { useState } from 'react';
+
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  // Event handling
-  // const handleClick = () => {
-  //   console.log('clicked');
-  // };
-
-  // from Stateful component
-  // setTimeout(() => setCounter(counter + 1), 1000);
-
   const increaseByOne = () => setCounter(counter + 1);
-
+  const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 };
