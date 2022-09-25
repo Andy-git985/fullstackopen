30972080b43344f7e90a6c7ac81a4e53da07808d
Andy-git985 <andy.yu617@gmail.com>
2022-09-25 19:44:36 -0400

    Handling arrays

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,50 @@
-// d: A more complex state, debugging React apps: Complex state
+// d: A more complex state, debugging React apps: Handling arrays
 import { useState } from 'react';
 
+// Complex state
+// const App = () => {
+//   const [clicks, setClicks] = useState({ left: 0, right: 0 });
+
+//   const handleLeftClick = () => {
+//     setClicks({ ...clicks, left: clicks.left + 1 });
+//   };
+
+//   const handleRightClick = () => {
+//     setClicks({ ...clicks, right: clicks.right + 1 });
+//   };
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <button onClick={handleLeftClick}>left</button>
+//       <button onClick={handleRightClick}>right</button>
+//       {clicks.right}
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1 });
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
   };
 
   const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 });
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
   };
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   );
 };
