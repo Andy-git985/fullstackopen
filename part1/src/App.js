03b747c56d53bd840701802abc6cee7174b5b0fa
Andy-git985 <andy.yu617@gmail.com>
2022-09-25 11:47:29 -0400

    Page re-rendering

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,37 +1,43 @@
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  );
-};
+// const Footer = () => {
+//   return (
+//     <div>
+//       greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+//     </div>
+//   );
+// };
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age;
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   );
+// };
 
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
-  return (
-    <>
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
-        <Footer />
-      </div>
-    </>
-  );
+// const App = () => {
+//   const name = 'Peter';
+//   const age = 10;
+//   return (
+//     <>
+//       <div>
+//         <h1>Greetings</h1>
+//         <Hello name="Maya" age={26 + 10} />
+//         <Hello name={name} age={age} />
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// };ss
+
+//Page re-rendering
+const App = (props) => {
+  const { counter } = props;
+  return <div>{counter}</div>;
 };
 
 export default App;
