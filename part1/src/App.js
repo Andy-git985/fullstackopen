d836c488bf74aa13288e747ab4810367b2f40d8a
Andy-git985 <andy.yu617@gmail.com>
2022-09-25 11:33:41 -0400

    Component helper functions

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,12 +5,19 @@ const Footer = () => {
     </div>
   );
 };
+
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
