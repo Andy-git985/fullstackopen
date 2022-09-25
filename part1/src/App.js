642db2e8308247eac8f9b5862a9913632057431b
Andy-git985 <andy.yu617@gmail.com>
2022-09-25 19:52:54 -0400

    Conditonal rendering

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,16 @@
-// d: A more complex state, debugging React apps: Handling arrays
+// d: A more complex state, debugging React apps: Conditional rendering
 import { useState } from 'react';
 
-// Complex state
-// const App = () => {
-//   const [clicks, setClicks] = useState({ left: 0, right: 0 });
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
 
-//   const handleLeftClick = () => {
-//     setClicks({ ...clicks, left: clicks.left + 1 });
-//   };
-
-//   const handleRightClick = () => {
-//     setClicks({ ...clicks, right: clicks.right + 1 });
-//   };
-
-//   return (
-//     <div>
-//       {clicks.left}
-//       <button onClick={handleLeftClick}>left</button>
-//       <button onClick={handleRightClick}>right</button>
-//       {clicks.right}
-//     </div>
-//   );
-// };
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   const [left, setLeft] = useState(0);
@@ -41,10 +30,10 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   );
 };
