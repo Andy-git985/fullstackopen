461d07cac5e6080abb79ad6388f1334d998475ce
Andy-git985 <andy.yu617@gmail.com>
2022-10-05 21:21:49 -0400

    a: Node.js and Express -Receiving data

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
-// a: Node.js and Express -Fetching a single resource
+// a: Node.js and Express -Receiving data
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 let notes = [
   {
@@ -39,6 +41,38 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
 });
 
 const PORT = 3001;
