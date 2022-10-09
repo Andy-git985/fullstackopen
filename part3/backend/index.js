bbb9995c43fe74cc1ca2802e0f623d56dfbeb524
Andy-git985 <andy.yu617@gmail.com>
2022-10-09 04:05:49 -0400

    cors static build

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,7 @@
 // a: Node.js and Express -Middlware
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -10,6 +11,8 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
 
@@ -90,6 +93,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
